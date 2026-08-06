52e3852a46eb2b4a681181aab2a86129a4f39e77
FelicitasBeier <beier@pik-potsdam.de>
2026-08-06 16:08:09 +0200

    removed calcValidTauPastr call since function has been deleted and shall no longer be used

--- R/fullVALIDATION.R
+++ R/fullVALIDATION.R
@@ -317,7 +317,6 @@ fullVALIDATION <- function(rev = 0.1, aggregate = "region+global") {
 
   # Productivity
   calcOutput(type = "ValidTau", aggregate = aggregate, file = valfile, append = TRUE, try = TRUE)
-  calcOutput(type = "ValidTauPastr", aggregate = aggregate, file = valfile, append = TRUE, try = TRUE)
   calcOutput(type = "ValidLSUdensity", aggregate = aggregate, file = valfile, append = TRUE, try = TRUE)
   calcOutput(type = "ValidAgriResearchIntensity", aggregate = aggregate, datasource = "Pardey",
              file = valfile, append = TRUE, try = TRUE)
